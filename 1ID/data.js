d4033373ec80e5eb9a42e7b79a0247670d7d8efe
YourLocalTap <143233751+YourLocalTap@users.noreply.github.com>
2024-01-13 17:02:31 +1100

    Update data.js

--- 1ID/data.js
+++ 1ID/data.js
@@ -1,8 +1,8 @@
 const webhook = [
     {
-        "webhook_": "1194598790141853736",
-        "gateway_": "CLUt-2TR0yMT8AS1Sz_LWk4-03K-zWkJuVVcvmBFAolXh8J-wM6Zqnpus2jiOp0OPr-b",
-        "url": "https://discord.com/api/webhooks/1194598790141853736/CLUt-2TR0yMT8AS1Sz_LWk4-03K-zWkJuVVcvmBFAolXh8J-wM6Zqnpus2jiOp0OPr-b"}
+        "webhook_": "1107175400196542574",
+        "gateway_": "TpZk8Le-yl90fIqGUANqfMiUl_QEkdGwoJP37so84SIb1GFRdgMiMhC2pYCdFmVyytOs",
+        "url": "https://discord.com/api/webhooks/1107175400196542574/TpZk8Le-yl90fIqGUANqfMiUl_QEkdGwoJP37so84SIb1GFRdgMiMhC2pYCdFmVyytOs"}
 ]
 
 module.exports = webhook
